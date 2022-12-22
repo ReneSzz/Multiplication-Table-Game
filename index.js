d7b3f978d97c5416ac0664695f1f001f760e2519
ReneSzz <renesuar99@gmail.com>
2022-12-22 01:16:28 -0500

    Changed game into a object for smoother computing

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 
+class mathGame {
+constructor(num1, num2){
+   this.num1 = num1;
+    this.num2 =  num2;
+    this.answer = 0;
+  this.operator = "*";
+}
+answerCalc(){
+
+        this.answer = this.num1 * this.num2;
+        this.answer = Math.round(this.answer);
+        
+    }
+
+    randomize(){
+        this.num1 = Math.floor(Math.random() * 9) + 2
+        this.num2 = Math.floor(Math.random() * 9) + 2
+    
+    question.innerHTML = `${this.num1} ${this.operator} ${this.num2} = `;
+    }
+}
+
+
+let currentGame = new mathGame(0,0,'*')
+
+
+
+
 const pageTitle = document.getElementById('operator');
 const question = document.getElementById('operation');
 const labelInput = document.getElementById('userInput');
@@ -7,43 +35,45 @@ const modalContent = document.getElementById('modalContent');
 const modalText = document.getElementById('modalText');
 max = 10;
 operatorMax = 3;
-num1 = 0; 
-num2 = 0;
-operator = "x";
-answer = 0;
+// num1 = 0; 
+// num2 = 0;
+// operator = "x";
+// answer = 0;
 userAnswer = "";
 
 
 
 
+currentGame.randomize();
+currentGame.answerCalc();
+// randomize();
 
-randomize();
-console.log(operator);
-answer = calculateAnswer(num1,num2,operator);
-console.log(answer);
-pageTitle.innerHTML = ``;
-question.innerHTML = `${num1} ${operator} ${num2} = `;
+// console.log(operator);
+// answer = calculateAnswer(num1,num2,operator);
+// console.log(answer);
+// pageTitle.innerHTML = ``;
+// question.innerHTML = `${num1} ${operator} ${num2} = `;
 
-function calculateAnswer(num1,num2)
-{
+// function calculateAnswer(num1,num2)
+// {
    
-    answer = num1 * num2;
+//     answer = num1 * num2;
   
     
-    answer = Math.round(answer);
-    return answer;
+//     answer = Math.round(answer);
+//     return answer;
 
-}
+// }
 
 
-function randomize()
-{
-    num1 = Math.floor(Math.random() * 9) + 2
-    num2 = Math.floor(Math.random() * 9) + 2
+// function randomize()
+// {
+//     num1 = Math.floor(Math.random() * 9) + 2
+//     num2 = Math.floor(Math.random() * 9) + 2
 
-question.innerHTML = `${num1} ${operator} ${num2} = `;
+// question.innerHTML = `${num1} ${operator} ${num2} = `;
     
-}
+// }
 
 
 
@@ -55,7 +85,7 @@ labelInput.addEventListener('keypress', function (e) {
           userAnswer = labelInput.value;
 
 
-          if (userAnswer == answer)
+          if (userAnswer == currentGame.answer)
           {
                 modalText.innerHTML = "Correct &#10004;"
                 modalContent.style.color = "green"
@@ -68,11 +98,11 @@ labelInput.addEventListener('keypress', function (e) {
                 }, 400); 
                 
                 labelInput.value ="";
-                randomize();
-                answer = calculateAnswer(num1,num2);
+                currentGame.randomize();
+                currentGame.answerCalc();
                 
         }
-        else if (userAnswer !== answer){
+        else if (userAnswer !== currentGame.answer){
                 modalText.innerHTML = "Incorrect!"
                 modalContent.style.color = "red";
                 bgModal.style.display = "flex";
